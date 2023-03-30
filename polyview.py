eda661e02df527a5479ad27b16dc1c0b8dce794b
Henning <hbruhn@gmx.net>
2023-03-30 11:48:39 +0200

    visual tool for polytopes and simplex algo

---
+++ polyview.py
@@ -0,0 +1,339 @@
+
+import cdd # pycddlib
+import numpy as np
+import plotly.graph_objects as go
+import math
+import ipywidgets as widgets
+import simplex
+
+###################### compute vertices and edges of polyhedron ##################
+
+def H_rep(A,b):
+    return np.hstack([b,-A])
+
+def find_face_vxs(vxs,poly,face_num,eps=1E-8):
+    ineq=np.array(poly.get_inequalities())[face_num]
+    face_vxs=[]
+    for vx in vxs:
+        if np.abs(ineq[0]+np.sum(ineq[1:]*vx))<eps:
+            face_vxs.append(vx)
+    return np.array(face_vxs)
+
+class Polyhedron:
+    def __init__(self,A,b):
+        A=np.array(A)
+        b=np.array(b).reshape(-1,1)
+        self.cdd_poly=self._get_polyhedron(A,b)
+        self.vertices=self._get_vertices()
+        self.edges=self._get_edges()
+        self.m,self.n=A.shape
+        self.face_vxs=[find_face_vxs(self.vertices,self.cdd_poly,face_num) for face_num in range(self.m)]
+        
+    def _get_polyhedron(self,A,b):
+        matrix=cdd.Matrix(H_rep(A,b))
+        matrix.rep_type=cdd.RepType.INEQUALITY
+        return cdd.Polyhedron(matrix)
+    
+    def _get_vertices(self):
+        generators=np.array(self.cdd_poly.get_generators())
+        vertices=generators[generators[:,0]==1]
+        vertices=vertices[:,1:]
+        if len(vertices)!=len(generators):
+            raise Exception("polyhedron unbounded!")
+        return vertices
+        
+    def _get_edges(self):
+        edges=[]
+        for vx_index,neighbours in enumerate(self.cdd_poly.get_adjacency()):
+            for neighbour_index in neighbours:
+                if vx_index<neighbour_index: # don't add edges twice
+                    edges.append(np.array([self.vertices[vx_index],self.vertices[neighbour_index]]))
+        return edges
+    
+        
+
+################### visualisation code #######################################
+
+def perturb(vxs):
+    """
+    Mesh3d doesn't seem to work properly if vxs are in same plane
+    for this reason we jitter the vxs a little bit
+    """
+    n=len(vxs)
+    return vxs+np.random.random(size=(n,3))*1E-4
+
+def objective_text(vertices,objective_vec):
+    return ["obj: "+str(sum(vx.flatten()*objective_vec.flatten())) for vx in vertices]
+        
+def highlight_face(poly,face_num,fig,visible=True):
+    colour_face(fig,poly.face_vxs[face_num],face_num=face_num,visible=visible)
+    
+def compute_range(vxs,axis_num):
+    ax_min,ax_max=min(vxs[:,axis_num]),max(vxs[:,axis_num])
+    spread=ax_max-ax_min
+    offset=spread*0.2
+    return [ax_min-offset,ax_max+offset]
+
+def colour_face(fig,face_vxs,face_num=-1,color="lightpink",visible=True,name=None):
+    if name is not None:
+        name=name
+    else:
+        name="face_"+str(face_num)
+    n=len(face_vxs)
+    if n==0:
+        return ## no use colouring a face consisting of just an edge or a vertex
+    if n==1:
+        fig.add_trace(go.Scatter3d(x=face_vxs[:,0],y=face_vxs[:,1],z=face_vxs[:,2],mode="markers",marker=dict(color=color),
+                              name=name,visible=visible))
+    elif n==2: 
+        fig.add_trace(go.Scatter3d(x=face_vxs[:,0],y=face_vxs[:,1],z=face_vxs[:,2],mode="lines",hoverinfo="skip",line=dict(width=40,color=color),
+                                  opacity=0.8,name=name,visible=visible))        
+    elif n==3: # for some reason alphahull=0 does not work if face is just a triangle
+        #print("triangle!")
+        fig.add_trace(go.Mesh3d(x=face_vxs[:,0],y=face_vxs[:,1],z=face_vxs[:,2],color=color,hoverinfo="skip",
+                            opacity=0.5,flatshading=True,i=[0],j=[1],k=[2],name=name,visible=visible))
+    else:
+        jitter_vxs=perturb(face_vxs) # Mesh3d has troubles showing flat face
+        fig.add_trace(go.Mesh3d(x=jitter_vxs[:,0],y=jitter_vxs[:,1],z=jitter_vxs[:,2],color=color,hoverinfo="skip",
+                            opacity=0.5, alphahull=0,flatshading=True,name=name,visible=visible))
+
+def plot_opt_path(fig,opt_path):
+    opt_path=np.array(opt_path)
+    #fig.add_trace(go.Scatter3d(x=opt_path[:,0],y=opt_path[:,1],z=opt_path[:,2],mode="lines",hoverinfo="skip",line=dict(width=10,color="red"),
+    #                          name="opt_path",visible=False))
+    fig.add_trace(go.Scatter3d(x=opt_path[0:1,0],y=opt_path[0:1,1],z=opt_path[0:1,2],mode="markers",marker=dict(color="red"),
+                              name="start_vx",visible=True))
+    for i in range(len(opt_path)-1):
+        fig.add_trace(go.Scatter3d(x=opt_path[i:i+2,0],y=opt_path[i:i+2,1],z=opt_path[i:i+2,2],mode="lines",hoverinfo="skip",line=dict(width=10,color="red"),
+                                  name="opt_path_"+str(i),visible=False))
+        
+    
+        
+def plot_poly(poly,objective_vec=None,opt_path=None,face_traces=False):
+    if objective_vec is not None:
+        text=objective_text(vxs,objective_vec)
+    else:
+        text=None
+    fig = go.Figure()
+    fig.add_trace(go.Scatter3d(x=poly.vertices[:,0],y=poly.vertices[:,1],z=poly.vertices[:,2],mode="markers",text=text))
+    for edge in poly.edges:
+        fig.add_trace(go.Scatter3d(x=edge[:,0],y=edge[:,1],z=edge[:,2],marker=dict(size=0), line=dict(width=5,color="blue"),hoverinfo="skip"))
+    if opt_path is not None:
+        plot_opt_path(fig,opt_path)
+    colour_face(fig,poly.vertices,color="lightblue",name="whole_poly")
+    if face_traces:
+        for face_num in range(poly.m):
+            highlight_face(poly,face_num,fig,visible=False)
+    xrange=compute_range(poly.vertices,0)
+    yrange=compute_range(poly.vertices,1)
+    zrange=compute_range(poly.vertices,2)
+    fig.update_layout(
+        width=800,
+        height=800,
+        autosize=False,
+        showlegend=False, 
+        scene = dict(xaxis = dict(range=xrange), yaxis = dict(range=yrange), zaxis = dict(range=zrange)    ),
+        paper_bgcolor="LightSteelBlue",
+            margin=dict(
+                l=10,
+                r=10,
+                b=50,
+                t=50,
+                pad=4
+            ),
+    )
+    #fig.show()
+    return fig
+
+def get_path(simplex):
+    return [state.x.flatten() for state in simplex.record]
+
+def factory_plot(factory,plot_path=False,face_num=None):
+    A,b,c,x0=factory()
+    poly=get_polyhedron(A,b)
+    if plot_path:
+        simplex=Simplex(A,b,c,x0,verbose=True)
+        plot_poly(poly,objective_vec=c,opt_path=get_path(simplex),face_num=face_num)
+    else:
+        plot_poly(poly,face_num=face_num)-1
+        
+        
+def list_str(l):
+    result="["
+    for item in l:
+        result+=str(round(item,3))+", "
+    return result[:-2]+"]"
+
+def with_sign(x):
+    r=round(x,2)
+    if r<0:
+        return str(r)
+    return "+"+str(r)
+
+def ineq_string(A,b,ineq_num):
+    a=A[ineq_num]
+    beta=np.array(b).flatten()[ineq_num]
+    return "{}x{}y{}z <= {}".format(round(a[0],2),with_sign(a[1]),with_sign(a[2]),round(beta,2))
+
+def setup_poly_viewer(A,b,*kwargs):
+    poly=Polyhedron(A,b)
+    out=widgets.Output(layout={'border':'6px solid LightSteelBlue','width': '90%', 
+                'height': '60px',})
+    fig=plot_poly(poly,face_traces=True)
+    fig_widget=go.FigureWidget(fig)
+    m,n=poly.m,poly.n
+    buttons=[
+        widgets.Checkbox(
+            value=False,
+            description=ineq_string(A,b,i),
+            disabled=False,
+            indent=False
+        ) for i in range(m)
+    ]
+
+    def face_output(face_num):
+        num_vxs_in_face=len(poly.face_vxs[face_num])
+        if num_vxs_in_face==0:
+            return "empty intersection"
+        if num_vxs_in_face==1:
+            return "vertex intersection"
+        if num_vxs_in_face==2:
+            return "edge intersection"
+        if num_vxs_in_face>=3:
+            return "facet intersection"
+
+    def on_check_factory(ineq_num):
+        def on_check(event):
+            with fig_widget.batch_update():
+                for trace in fig_widget.data:
+                    if trace.name[:4]=="face":
+                        if int(trace.name[5:])==ineq_num:
+                            trace.update(visible=event.new)
+            out.clear_output()
+            with out:
+                if event.new:
+                    print("inequality {} selected".format(ineq_num))
+                    print(face_output(ineq_num))
+                else:
+                    print("no inequality selected")
+        return on_check
+
+    for i,button in enumerate(buttons):
+        button.observe(on_check_factory(i),"value")
+
+    explainer=widgets.HTML(
+        value="Select inequalities to highlight intersection",
+        placeholder='Some HTML',
+        #description='Some HTML',
+    )    
+    vbox=widgets.VBox([explainer]+buttons+[out])
+    hbox=widgets.HBox([fig_widget,vbox])
+    with out:
+        print("no inequality selected")
+
+    return hbox    
+
+
+class Display_State:
+    def __init__(self,smplx,fig_widget,output,face_highlighting=True):
+        self.simplex=smplx
+        self.step_num=-1
+        self.max_step_num=len(self.simplex.record)
+        self.widget=fig_widget
+        self.out=output
+        self.forward_button=widgets.Button(icon="forward")
+        self.backward_button=widgets.Button(icon="backward",disabled=True)
+        self.forward_button.on_click(self.forward_func())
+        self.backward_button.on_click(self.backward_func())
+        self.do_face_highlighting=face_highlighting
+        
+    def complete_y(self,y_J,J):
+        m=self.simplex.m
+        y=np.zeros(m)
+        y[J]=y_J
+        return y
+    
+    def print_state(self):
+        self.out.clear_output()
+        with self.out:
+            print("Step number: {}".format(self.step_num))
+            state=self.simplex.record[self.step_num]
+            print("J={}".format(state.J))
+            print("x={}".format(list_str(state.x)))
+            print("objective value={}".format(round(state.objective,2)))
+            y=self.complete_y(state.y_J,state.J)
+            print("y={}".format(list_str(y)))
+            if state.opt_status!="OPT found":
+                print("i | j*= {} | {}".format(state.i,state.jstar))  
+            else:
+                print("optimum reached")
+
+    def update_path(self):
+        with self.widget.batch_update():
+            for trace in self.widget.data:
+                if trace.name[:8]=="opt_path":
+                    if int(trace.name[9:])<self.step_num:
+                        trace.update(visible=True)
+                    else:
+                        trace.update(visible=False)
+            if self.do_face_highlighting:
+                self.update_face_highlighting()
+                        
+    def update_face_highlighting(self):
+        state=self.simplex.record[self.step_num]
+        J=state.J
+        with self.widget.batch_update():
+            for trace in self.widget.data:
+                if trace.name[:4]=="face":
+                    if int(trace.name[5:]) in J:
+                        trace.update(visible=True)
+                    else:
+                        trace.update(visible=False)
+
+    def update_button_disable(self):
+        if self.step_num==self.max_step_num-1:
+            self.forward_button.disabled=True
+        if self.step_num>0:
+            self.backward_button.disabled=False
+        
+                        
+    def forward_func(self):
+        def forward(button):
+            if self.step_num<self.max_step_num-1:
+                self.step_num+=1
+                self.print_state()
+                self.update_path()
+            self.update_button_disable()
+        return forward
+            
+    def backward_func(self):
+        def backward(button):
+            if self.step_num>0:
+                self.forward_button.disabled=False
+                self.step_num-=1
+                self.print_state()
+                self.update_path()
+            self.update_button_disable()
+        return backward
+    
+def setup_simplex_widget(A,b,c,x0):
+    poly=Polyhedron(A,b)
+
+    smplx=simplex.Simplex(A,b,c,x0,verbose=True)
+    opt_path=get_path(smplx)    
+    out=widgets.Output(layout={'border':'6px solid LightSteelBlue','width': '90%', 
+                'height': '160px',})
+
+    fig=plot_poly(poly,opt_path=opt_path,face_traces=True)
+    fig_widget=go.FigureWidget(fig)
+
+    dstate=Display_State(smplx,fig_widget,out)
+    forward=dstate.forward_button
+    backward=dstate.backward_button
+    button_box=widgets.HBox([backward,forward])
+    vbox=widgets.VBox([button_box,out])
+    hbox=widgets.HBox([fig_widget,vbox])
+    with out:
+        print("Click buttons to perform simplex step")
+
+    return hbox
---
+++ simplex.py
@@ -0,0 +1,249 @@
+#################### The simplex algorithm ###############################
+
+# conceptual implementation, not suitable for performance
+# only for educational purpose
+
+import numpy as np
+import math
+
+class State:
+    """
+    simple class that keeps track of what's happening in each simplex step
+    the following parameters are stored:
+    x,J,y_J,i,j*,w,lambda,the new vertex x, the current objective value, whether the OPT was reached
+    """
+    def __init__(self,x=None,J=None,y_J=None,i=None,jstar=None,w=None,opt_status="running",lmbda=None,new_x=None,objective=None):
+        self.x=x.flatten()
+        self.J=J.copy() if J is not None else None # a little bit of python magic to cope with J==None
+        self.y_J=y_J.flatten() if y_J is not None else None 
+        self.i=i
+        self.jstar=jstar
+        self.w=w.flatten() if w is not None else None
+        self.opt_status=opt_status
+        self.lmbda=lmbda
+        self.new_x=new_x.flatten() if new_x is not None else None
+        self.objective=objective
+    
+    def __repr__(self):
+        repr=""
+        # only report those values that are not None
+        for key,value in vars(self).items():
+            if value is not None:
+                repr+="{}: {}\n".format(key,value)
+        return repr+"\n"
+
+def unit_vec(i,n):
+    """
+    return unit vector e_i of length n
+    """
+    vec=np.zeros(n)
+    vec[i]=1
+    return vec.reshape(-1,1)
+
+class Simplex:
+    def __init__(self,A,b,c,x0,max_iter=100,eps=1E-8,verbose=True):
+        """
+        solve max c^Tx s.t. Ax<=b
+        A: matrix of conditions
+        b: right-hand side
+        c: vector of objective function
+        x0: start vertex
+        max_iter: maximum number of simplex steps (default=100)
+        eps: numerical precision (default=10^-8)
+        verbose: True/False, whether to report optimisation result (default=True)
+        when finished, the following attributes are set
+        simplex.x: the optimal solution (if exists)
+        simplex.opt_status: string "OPT found" / "unbounded"
+        simplex.record: a list containing a State object for each simplex step
+        """
+        self.A=np.array(A) # make sure it's a numpy array
+        self.b=np.array(b).reshape(-1,1) # make sure it's a numpy array column vector
+        self.c=np.array(c).reshape(-1,1) # make sure it's a numpy array  column vector
+        self.x=np.array(x0).reshape(-1,1) # make sure it's a numpy array  column vector
+        m,n=self.A.shape
+        self.m=m
+        self.n=n
+        self.eps=eps
+        self.record=[]
+        self.done=False
+        self.max_iter=max_iter
+        self.check_start_vertex()
+        self.init_representation()
+        self.run()
+        if verbose:
+            self.report_result()
+        
+    def check_start_vertex(self):
+        """
+        check whether x0 is indeed feasible
+        """
+        # check whether A*x0<=b because of numerical imprecisions allow up to eps deviation
+        if not np.all(self.A@self.x-self.b <=self.eps): 
+            raise Exception("start parameter x0 not feasible!")
+        
+    def current_objective(self):
+        """
+        objective value of current solution simplex.x
+        """
+        return np.sum(self.x.T@self.c)
+        
+    def run(self):
+        """
+        run the actual algorithm
+        """
+        for _ in range(self.max_iter):
+            if self.done:
+                break
+            self.simplex_step()
+        if not self.done:
+            self.opt_status="max iteration reached"
+        
+    def init_representation(self):
+        """
+        compute representing index set J for start solution x0
+        """
+        # collect the indices of tight inequalities
+        # allow up to eps deviation due to numerical imprecisions
+        self.J=[j for j,a in enumerate(self.A) if abs(a@self.x-self.b[j])<self.eps]
+        # check whether A_J has indeed rank n
+        m,n=self.A.shape
+        if np.linalg.matrix_rank(self.A[self.J])<n:
+            raise Exception("start parameter x0 not a vertex!")
+        self.reduce_index_set() # throw away inequalities in J if |J|>n
+        
+    def reduce_by_one(self):
+        m,n=self.A.shape
+        for j in range(len(self.J)):
+            JJ=self.J.copy()
+            del JJ[j]
+            if np.linalg.matrix_rank(self.A[JJ])>=n:
+                break
+        del self.J[j]
+        
+    def reduce_index_set(self):
+        """
+        throw away inequalities in simplex.J if |J|>n
+        while making sure that still rank(A_J)=n
+        """
+        m,n=self.A.shape
+        while len(self.J)>n:
+            self.reduce_by_one()
+        
+    def simplex_step(self):
+        """
+        a single step of the simplex algorithm
+        """
+        ### 1: reduce matrix to the rows in J
+        A_J=self.A[self.J]
+        ### 2: compute y_J
+        self.y_J=np.linalg.solve(A_J.T,self.c)
+        obj=self.current_objective()                              # for record keeping
+        state=State(x=self.x,J=self.J,y_J=self.y_J,objective=obj) # for record keeping
+        self.record.append(state)                                 # for record keeping
+        ### 3: pick index i with y_i<0
+        index_i_found=False
+        for rel_i,yy in enumerate(self.y_J):
+            if yy<-self.eps: # is y_i<0? up to eps due to numerical imprecisions
+                index_i_found=True
+                break
+        ### 4: check whether optimum found
+        if not index_i_found:
+            self.done=True
+            self.opt_status="OPT found"
+            state.opt_status="OPT found"                          # for record keeping
+            return
+        state.i=self.J[rel_i]                                     # for record keeping
+        ### 5: compute w, direction in which objective value can be improved
+        w=np.linalg.solve(A_J,-unit_vec(rel_i,len(self.J)))
+        state.w=w.flatten()                                       # for record keeping
+        ### 6: check whether A*w<=0, ie whether LP is unbounded
+        if np.all(self.A@w<=self.eps):
+            self.done=True
+            self.opt_status="unbounded"
+            state.opt_status="unbounded"                          # for record keeping
+            return
+        ### 7: compute lambda and j*, the index to add to J
+        lambdas=[(sum((self.b[j]-sum(self.A[j]@self.x))/sum(self.A[j]@w)),j) for j in range(len(self.A)) if sum(self.A[j]@w)>self.eps]
+        lmbda,jstar=min(lambdas,key=lambda l:l[0])
+        state.jstar=jstar                                         # for record keeping
+        state.lmbda=lmbda                                         # for record keeping
+        ### 8: update J by deleting i and adding j*
+        self.J[rel_i]=jstar
+        self.J.sort()
+        ### 9: compute new vertex x defined by J
+        self.x=np.linalg.solve(self.A[self.J],self.b[self.J])
+        state.new_x=self.x.flatten()                              # for record keeping
+        
+    def report_result(self):
+        steps=len(self.record)
+        if self.opt_status=="unbounded":
+            print("LP unbounded")
+        if self.opt_status=="OPT found":
+            print("OPT found")
+            print(" at x={}".format(self.x.flatten()))
+            print(" OPT value={}".format(self.current_objective()))
+        if self.opt_status=="max iteration reached":
+            print("algorithm stopped as max number of iterations was reached")
+        print("simplex took {} steps".format(steps))
+ 
+###################### some sample linear programmes ############################     
+
+def add_nonneg(A,b):
+    """
+    add non-negativity constraints x>=0 to system Ax<=b
+    """
+    m,n=A.shape
+    b=np.vstack([b,np.zeros(n).reshape(-1,1)])
+    A=np.vstack([A,-np.eye(n)])
+    return A,b
+
+def twisted_cube(n=3):
+    c=np.array([2**i for i in range(n-1,-1,-1)]).reshape(-1,1)
+    b=np.array([5**i for i in range(1,n+1)]).reshape(-1,1)
+    A=2*np.array([[int(2**(i-j)) for j in range(n)] for i in range(n)])
+    for i in range(n):
+        A[i,i]=1
+    A,b=add_nonneg(A,b)
+    R=np.array([[i+n,i] for i in range(n)]).flatten()
+    A=A[R].copy()
+    b=b[R].copy()
+    x0=np.zeros(n).reshape(-1,1)
+    return A,b,c,x0
+
+def dodecahedron():
+    phi=0.5*(math.sqrt(5)+1)
+    generators=[[0,(-1)**i,(-1)**round(0.4*i)*phi] for i in range(4)]
+    ineqs=[]
+    for generator in generators:
+        for _ in range(3):
+            ineqs.append(generator.copy())
+            generator.append(generator.pop(0))
+    A=np.vstack(ineqs)
+    b=np.ones(len(A)).reshape(-1,1)
+    c=np.ones(3).reshape(-1,1)
+    x0=-np.array([1-1/phi]*3).reshape(-1,1)
+    return A,b,c,x0
+
+def octahedron():
+    A=np.array([[i,j,k] for i in [-1,1] for j in [-1,1] for k in [-1,1]])
+    b=np.ones(8).reshape(-1,1)
+    c=np.array([0,0,1]).reshape(-1,1)
+    x0=np.array([0,0,-1]).reshape(-1,1)
+    return A,b,c,x0
+
+def flat():
+    A=np.array([[2,-1,0],[1,0,0],[0,1,0],[-1,1,0],[-1,0,0],[0,-1,0],[1,1,-1],[-1,-1,1]])
+    b=np.array([2,3,5,4,0,0,0,0]).reshape(-1,1)
+    c=np.ones(3).reshape(-1,1)
+    x0=np.zeros(3).reshape(-1,1)
+    return A,b,c,x0
+
+def sample_poly():
+    cube_A,cube_b=add_nonneg(np.eye(3),4*np.ones(3).reshape(-1,1))
+    more_A=[[1,2,-1],[1.2,-3.2,1.6],[-1,-1,1],[-0.25,0,1]]
+    more_b=np.array([2,8,1.5,3]).reshape(-1,1)
+    A=np.vstack([cube_A,more_A])
+    b=np.vstack([cube_b,more_b])
+    c=np.array([0,0,1]).reshape(-1,1)
+    x0=np.zeros(3).reshape(-1,1)
+    return A,b,c,x0
